0b6f772453c6d75f6de8bfbdc2ca18df6d9acda7
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-01-25 16:41:39 -0600

    Configure environment variables

--- hello_prefect.py
+++ hello_prefect.py
@@ -1,10 +1,13 @@
+import os
 import prefect
 from prefect import task, Flow, Parameter
+from prefect.run_configs import LocalRun
 
 @task
 def hello_task(name):
+    greeting = os.environ.get("GREETING")
     logger = prefect.context.get("logger")
-    logger.info(f"Hello, {name}")
+    logger.info(f"{greeting}, {name}")
 
 # flow = Flow("hello-flow", tasks=[hello_task])
 with Flow("hello-flow") as flow:
@@ -13,6 +16,7 @@ with Flow("hello-flow") as flow:
     hello_task.map(people)
 
 # flow.run() This command is used for running locally
+flow.run_config = LocalRun(env={"GREETING": "Hello"})
 
 flow.register(project_name="operationalize-ml-microservice")
 flow.run_agent()
