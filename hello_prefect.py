2f9c9d9c4876a2823e55f73c5dcb09a60ceee408
Antonette Caldwell <antonette.caldwell@outlook.com>
2021-01-25 16:38:13 -0600

    Next version flow: add parameters for people's names

--- hello_prefect.py
+++ hello_prefect.py
@@ -1,12 +1,16 @@
 import prefect
-from prefect import task, Flow
+from prefect import task, Flow, Parameter
 
 @task
-def hello_task():
+def hello_task(name):
     logger = prefect.context.get("logger")
-    logger.info("Hello world!")
+    logger.info(f"Hello, {name}")
 
-flow = Flow("hello-flow", tasks=[hello_task])
+# flow = Flow("hello-flow", tasks=[hello_task])
+with Flow("hello-flow") as flow:
+    people = Parameter("people", default=["Arthur", "Ford", "Marvin"])
+
+    hello_task.map(people)
 
 # flow.run() This command is used for running locally
 
